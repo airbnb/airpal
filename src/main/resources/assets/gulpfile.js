fedff66f098a28f7283c928e0c903bc51becc01f
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-10-10 15:58:25 -0700

    Updates the usage of watchify
    * Removes the need for watchify, because gulp watch and watchify don't
    * play very well together.

--- src/main/resources/assets/gulpfile.js
+++ src/main/resources/assets/gulpfile.js
@@ -6,7 +6,6 @@ gulp        = require('gulp'),
 gutil       = require('gulp-util'),
 source      = require('vinyl-source-stream'),
 buffer      = require('vinyl-buffer'),
-watchify    = require('watchify'),
 browserify  = require('browserify'),
 reactify    = require('reactify'),
 uglify      = require('gulp-uglify');
@@ -18,23 +17,12 @@ paths = {
 
 // Create a "browserify" task
 gulp.task('browserify', function() {
-
-  // Define the browserify bundler
-  bundler = browserify('./js/app.js', {
+  browserify('./js/app.js', {
     transform: [reactify],
     cache: {},
     packageCache: {},
     fullPaths: false
-  });
-
-  // Define or the bundler should use the watchify command or
-  // just use browserify
-  if( typeof gutil.env.watchify === 'undefined' || !gutil.env.watchify ) {
-    bundler = watchify(bundler);
-  }
-
-  // Configure the actions on the bundler
-  bundler
+  })
     .bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('main.js'))
