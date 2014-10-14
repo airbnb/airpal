e0cc76f47876c1495d02f2ed8eaaa15b8d599c00
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-10-14 14:19:55 -0700

    Completely refactored the editor.js file. Now it's more readable and cleaner.

--- src/main/resources/assets/js/elements/editor.js
+++ src/main/resources/assets/js/elements/editor.js
@@ -1,189 +1,223 @@
 /** @jsx React.DOM */
+var React           = require('react/addons'),
+    QueryEditor     = require('./query_editor'),
+    SavedQueryForm  = require('./saved_query_form'),
+    _               = require('lodash'),
+    ErrorMessage    = require('./error_message');
 
-var React = require('react/addons'),
-    QueryEditor = require('./query_editor'),
-    SavedQueryForm = require('./saved_query_form'),
-    _ = require('lodash'),
-    cx = React.addons.classSet,
-    Editor;
-
-function hasMultipleStatements(queryData) {
-  var statementTerminateRE = /;$/m,
-      newLineRE = /\r?\n/,
-      queryStatements = _.reject(
-        queryData.replace(newLineRE, '').split(statementTerminateRE),
-        function(val) {
-          return _.isEmpty(val);
-        }),
-      newQuery;
-
-  return (queryStatements.length >= 2 && !_.isEmpty(queryStatements[1]));
-}
-
-Editor = React.createClass({
+var Editor = React.createClass({
   getInitialState: function() {
     return {
-      showAlert: this.props.showAlert,
       runText: this.props.runText,
-      saveModalOpen: false,
       modalQuery: '',
       querySaving: false,
+
+      statementError: {
+        visible: false,
+        message: 'Airpal does not currently support multiple <a href="#" data-toggle="tooltip" title="A statement is a SQL expression terminated by a semicolon (;)">statements</a> in a query. Please re-phrase your query and try again.'
+      },
+
+      saveError: {
+        visible: false,
+        message: ''
+      },
+
+      runError: {
+        visible: false,
+        message: ''
+      }
     };
   },
+
   getDefaultProps: function() {
     return {
-      showAlert: false,
       runText: 'Query',
       onQueryRun: function(query) {},
       onQuerySave: function(savedQuery) {},
     };
   },
-  render: function() {
-    var alertClasses = cx({
-      'panel-header': true,
-      'alert': true,
-      'alert-header': true,
-      'alert-danger': true,
-      'hide': !this.state.showAlert
-    }), saveIconClasses = cx({
-      'save-loading': true,
-      'hide': !this.state.querySaving,
-    });
 
-    return (<div>
-      <div className="row-12 row-space-top-2">
-        <div className="panel query-panel">
-          <div>
-            <div className="panel-header">Query</div>
-            <p className={alertClasses} role="alert">
-                <a href="#" className="alert-close" onClick={this.handleCloseAlert}></a>
-                Airpal does not currently support multiple
-                <span className="header-tip"
-                      data-behavior="tooltip"
-                      data-position="top"
-                      title="A statement is a SQL expression terminated by a semicolon (;)">statements</span>
-                    in a query. Please re-phrase your query and try again.
-            </p>
+  render: function() {
+
+    // Set the right CSS classes
+    this._updateCSSClasses();
+
+    // Return the template
+    return (
+      <div className="row">
+
+        <div className="col-sm-12">
+          <div className="panel panel-default query-panel">
+
+            <div className="panel-heading">
+              <strong>Query</strong>
+            </div>
+
+            <div className="panel-body">
+              <ErrorMessage visible={this.state.statementError.visible} message={this.state.statementError.message} />
+              <ErrorMessage visible={this.state.runError.visible} message={this.state.runError.message} />
+
+              <QueryEditor ref="editor" defaultQuery="SELECT COUNT(1) FROM users" onSelection={this.handleEditorSelection} />
+            </div>
+
           </div>
-          <QueryEditor
-              defaultQuery="SELECT COUNT(1) FROM users"
-              onSelection={this.handleEditorSelection}
-              ref="editor" />
         </div>
-      </div>
-      <div className="row-12 row-space-top-2 clearfix">
-        <div className="col-6 pull-left run-query-form run-query">
-          <input ref="tempTable" class="pull-left" placeholder="Temporary Table Name" type="text"/>
+
+        <div className="col-sm-12">
+          <div className="row">
+
+            <div className="col-sm-6 text-left run-query-form run-query">
+              <input ref="tempTable" className="form-control pull-left" placeholder="Temporary Table Name" type="text" />
+            </div>
+
+            <div className="col-sm-6 text-right">
+              <button type="button" className="btn btn-primary" onClick={this.handleOpenModal}>Save {this.state.runText}</button>
+              <button type="button" className="btn btn-primary" onClick={this.handleRunClick}>Run {this.state.runText}</button>
+            </div>
+
+          </div>
         </div>
-        <div className="pull-right">
-          <button
-            className="btn btn-primary btn-large"
-            onClick={this.handleSaveClick}>Save {this.state.runText}</button>
-          <button
-            className="btn btn-primary btn-large"
-            onClick={this.handleRunClick}>Run {this.state.runText}</button>
-        </div>
-      </div>
-      <div className="modal"
-           aria-hidden={!this.state.saveModalOpen}
-           ref="saveQueryModal">
-        <div className="modal-table">
-          <div className="modal-cell">
+
+        <div id="saveQueryModal" ref="saveQueryModal" className="modal fade"  tabindex="-1" role="dialog" aria-labelledby="saveQueryModal" aria-hidden="true">
+          <div className="modal-dialog">
             <div className="modal-content">
-              <div className="panel-header">
-                <a href="#" className="panel-close" onClick={this.handleCloseModal}></a>
-                Save Query
+
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">
+                  <span aria-hidden="true">&times;</span> <span className="sr-only">Close</span>
+                </button>
+
+                <h4 className="modal-title">Save Query</h4>
               </div>
-              <div className="panel-body">
-                <SavedQueryForm
-                  query={this.state.modalQuery}
-                  onSubmit={this.handleSaveSubmit}
-                  ref="savedQueryForm"
-                  context="Save" />
+
+              <div className="modal-body">
+                <ErrorMessage visible={this.state.saveError.visible} message={this.state.saveError.message} />
+
+                <SavedQueryForm ref="savedQueryForm" query={this.state.modalQuery} onSubmit={this.handleSaveSubmit} context="Save" />
               </div>
-              <div className="panel-footer">
-                <div className={saveIconClasses}>
-                  <i className="icon icon-repeat"></i>
-                </div>
-                <button className="btn" onClick={this.handleCloseModal}>Cancel</button>
-                <button className="btn btn-primary" onClick={this.handleSaveSubmit}>Save</button>
+
+              <div className="modal-footer">
+                  <div className={this.classes.icon}>
+                    <span className="glyphicon glyphicon-repeat"></span> Saving query...
+                  </div>
+
+                  <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+                  <button type="button" className="btn btn-primary" onClick={this.handleSaveSubmit}>Save Query</button>
               </div>
+
             </div>
           </div>
         </div>
+
       </div>
-    </div>);
+    );
   },
 
-  handleCloseAlert: function(e) {
-    e.preventDefault();
-    this.setState({showAlert: false});
-  },
-  checkStatement: function() {
-    var hasMultiple = hasMultipleStatements(this.refs.editor.getValue()) && false;
-    console.log('check statement', 'has multiple', hasMultiple, this, arguments);
-    this.setState({
-      showAlert: hasMultiple
-    });
-  },
-  handleSaveClick: function(e) {
-    this.checkStatement();
-    e.preventDefault();
+  /* Internal Helpers ------------------------------------------------------- */
+  _updateCSSClasses: function() {
+    this.classes || (this.classes = {})
 
-    this.setState({
-      modalQuery: this.refs.editor.getQuery(),
-      saveModalOpen: true,
+    // Set all the save icon classes
+    this.classes['icon'] = React.addons.classSet({
+      'save-loading': true,
+      'pull-left': true,
+      'hide': !this.state.querySaving
     });
   },
+
+  _hasMultipleStatements: function(queryData) {
+    var statementTerminateRE = /;$/m,
+        newLineRE = /\r?\n/,
+        queryStatements = _.reject(
+          queryData.replace(newLineRE, '').split(statementTerminateRE),
+          function(val) {
+            return _.isEmpty(val);
+          }),
+        newQuery;
+
+    return (queryStatements.length >= 2 && !_.isEmpty(queryStatements[1]));
+  },
+
+  _checkStatement: function() {
+    if( !this._hasMultipleStatements(this.refs.editor.getValue()) ) { return true; }
+
+    // There are multiple statements, so show an error to the user
+    this.setState({
+      statementError: _.extend(this.state.statementError, { visible: true })
+    });
+
+    // Prevent the parent function from continuing
+    return false;
+  },
+
+  /* Event Handlers --------------------------------------------------------- */
+  handleOpenModal: function($event) {
+    $event.preventDefault();
+    this._checkStatement(); // Check if we should continue
+
+    // Grab the current query for the modal
+    this.setState({ modalQuery: this.refs.editor.getQuery() })
+
+    // Get the modal and open it
+    var modal = this.refs.saveQueryModal.getDOMNode();
+    $(modal).modal('show');
+  },
+
   handleEditorSelection: function(isRange, query) {
     this.setState({
       runText: isRange ? 'Selected' : 'Query',
     });
   },
-  handleRunClick: function(e) {
+
+  handleRunClick: function($event) {
+    $event.preventDefault();
+    this._checkStatement(); // Check if we should continue
+
+    // Get tje temporary table
     var tmpTable = this.refs.tempTable.getDOMNode().value;
 
-    console.log('handleRunClick', this, arguments);
-    this.checkStatement();
-    e && e.preventDefault();
-
+    // Make the execute call
     $.ajax({
       url: '/api/execute',
       type: 'PUT',
       contentType: 'application/json',
-      error: function() {
-        console.log('Saw error from EXECUTE', arguments);
-      },
-      success: function(data) {
-        if (_.isEmpty(data) || _.isEmpty(data.uuid)) {
-          return;
-        }
-        this.setState({
-          activeQuery: data.uuid,
-        });
-      }.bind(this),
       data: JSON.stringify({
         query: this.refs.editor.getQuery(),
         tmpTable: _.isEmpty(tmpTable) ? null : tmpTable
-      })
+      }),
+
+      error: function(response, textStatus, error) {
+
+        // Update the visible state and the error message for
+        // running the query
+        this.setState({
+          runError: { visible: true, message: error }
+        });
+      }.bind(this),
+
+      success: function(data) {
+        if (_.isEmpty(data) || _.isEmpty(data.uuid)) { return; }
+
+        // Set the active query state
+        this.setState({ activeQuery: data.uuid });
+      }.bind(this)
     });
   },
-  handleCloseModal: function(e) {
-    e.preventDefault();
-    this.setState({
-      saveModalOpen: false,
-    });
-  },
-  handleSaveSubmit: function(e) {
-    e && e.preventDefault();
+
+  handleSaveSubmit: function($event) {
+    $event.preventDefault();
+
+    // Show the user that the request is pending by setting
+    // the querySaving state to true
     this.setState({ querySaving: true });
 
-    var name = this.refs.savedQueryForm.refs.name.getDOMNode().value,
-        description = this.refs.savedQueryForm.refs.description.getDOMNode().value,
-        query = this.refs.savedQueryForm.props.query;
+    // Get the data to submit
+    var refs        = this.refs.savedQueryForm.refs,
+        name        = refs.name.getDOMNode().value,
+        description = refs.description.getDOMNode().value,
+        query       = this.refs.savedQueryForm.props.query;
 
-    console.log('[Editor#handleSaveSubmit]', name, description, query, e, this);
-
+    // Post the query to save it
     $.ajax({
       url: '/api/query/saved',
       type: 'POST',
@@ -191,26 +225,39 @@ Editor = React.createClass({
         query: query,
         name: name,
         description: description
-      }
-    }).done(function(data, textStatus, jqXHR) {
-      this.setState({
-        saveModalOpen: false,
-      });
-      this.props.onQuerySave({
-        queryWithPlaceholders: {query: query},
-        description: description,
-        name: name,
-        uuid: data,
-      });
-    }.bind(this)).fail(function(jqXHR, textStatus, errorThrown) {
-      console.log('save query failed', jqXHR, textStatus, errorThrown);
-    }).always(function(dataOrJqXHR, textStatus, jqXHROrErrorThrown) {
+      },
+
+      success: function() {
+
+        // Notify the parent that the query has been
+        // saved to the server
+        this.props.onQuerySave({
+          queryWithPlaceholders: { query: query },
+          description: description,
+          name: name,
+          uuid: data
+        });
+
+        // Get the modal and close it
+        var modal = this.refs.saveQueryModal.getDOMNode();
+        $(modal).modal('hide');
+      }.bind(this),
+
+      error: function(response, textStatus, error) {
+
+        // Update the visible state and the error message for
+        // saving the query
+        this.setState({
+          saveError: { visible: true, message: error }
+        });
+      }.bind(this)
+
+    }).always(function() {
+
+      // Stop notifying the user the request is pending
       this.setState({ querySaving: false });
     }.bind(this));
-  },
-  setValue: function(value) {
-    return this.refs.editor.setValue(value);
-  },
+  }
 });
 
 module.exports = Editor;
