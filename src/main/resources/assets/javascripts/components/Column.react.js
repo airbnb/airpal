8d767ecf16de82d84077eecdfc8a47840fcebc42
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-11-22 22:49:27 +0100

    Adds a column preview, data preview and meta data preview

---
+++ src/main/resources/assets/javascripts/components/Column.react.js
@@ -0,0 +1,35 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+/* Helpers */
+var _     = require('lodash');
+
+var Column = React.createClass({
+  displayName: 'Column',
+
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    type: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+
+    // Define the output value
+    var value = this.props.partition ? '(Partition)' : '(' + this.props.type + ')';
+
+    // Return the template
+    return (
+      <div className="col-sm-3">
+        <div className="panel panel-default">
+
+          <div className="panel-body">
+            <strong>{this.props.name} </strong> {value}
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Column;
