058751334b523f5bb8323f6d2c15489d175756d2
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-11-22 14:54:10 +0100

    Removes old JS files

--- src/main/resources/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-require('./lib/jquery.cookie');
-require('./lib/jquery.event.drag-2.2');
-require('./lib/slick.core');
-require('./lib/slick.grid');
-
-var InitReact = require('./app2');
-$(InitReact);
