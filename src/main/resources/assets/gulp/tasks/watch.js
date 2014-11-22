6b73d7120b69869d8f6f3f14b62c995c5c83ea42
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-11-22 11:20:42 +0100

    Finished the new frontend implementation

---
+++ src/main/resources/assets/gulp/tasks/watch.js
@@ -0,0 +1,9 @@
+/* Notes:
+   - gulp/tasks/browserify.js handles js recompiling with watchify
+   - gulp/tasks/browserSync.js watches and reloads compiled files
+*/
+
+var gulp  = require('gulp');
+var config= require('../config');
+
+gulp.task('watch', ['setWatch', 'browserSync']);
