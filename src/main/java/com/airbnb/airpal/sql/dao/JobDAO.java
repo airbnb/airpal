3ae78a813a0c5d387ccb1fcf4130b5239fce748b
Andy Kramolisch <a.r.kramolisch@gmail.com>
2014-11-25 17:18:50 -0800

    [sql] TIMESTAMP -> DATETIME and bump mysql connector

--- src/main/java/com/airbnb/airpal/sql/dao/JobDAO.java
+++ src/main/java/com/airbnb/airpal/sql/dao/JobDAO.java
@@ -9,7 +9,16 @@ public interface JobDAO
 {
     @SqlUpdate(
             "INSERT INTO jobs (query, user, uuid, queryStats, state, columns, query_finished, query_started, error) " +
-            "VALUES (:query, :user, :uuid, :queryStats, :state, :columns, FROM_UNIXTIME( :queryFinished ), FROM_UNIXTIME( :queryStarted ), :error)")
+            "VALUES (" +
+                    ":query, " +
+                    ":user, " +
+                    ":uuid, " +
+                    ":queryStats, " +
+                    ":state, " +
+                    ":columns, " +
+                    "FROM_UNIXTIME(ROUND(:queryFinished / 1000)), " +
+                    "FROM_UNIXTIME(ROUND(:queryStarted / 1000)), " +
+                    ":error)")
     @GetGeneratedKeys
     long createJob(
             @RosettaBinder Job job
