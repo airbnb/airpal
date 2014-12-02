2fd9e0209a2f0dec9045772ced6500940afb06c7
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-12-02 23:20:09 +0100

    Creates a custom modal

--- src/main/resources/assets/javascripts/components/QuerySaveModal.react.js
+++ src/main/resources/assets/javascripts/components/QuerySaveModal.react.js
@@ -1,26 +1,85 @@
 /** @jsx React.DOM */
 var React = require('react');
 
+/* Actions */
+var QueryActions = require('../actions/QueryActions');
+
 /* Helpers */
-var
+var Modal = require('react-bootstrap').Modal;
+
+/* Stores */
+var QueryStore = require('../stores/QueryStore');
 
 var QuerySaveModal = React.createClass({
   displayName: 'QuerySaveModal',
+
+  componentDidMount: function() {
+    QueryStore.addStoreListener('add', this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    QueryStore.removeStoreListener('add', this._onChange);
+  },
+
   render: function () {
     return (
-      <Modal {...this.props} title="Modal heading">
-        <div className="modal-body"></div>
+      <Modal {...this.props} title="Save a new query">
+        <div className="modal-body">
+          <form className="form-horizontal" action="#" onSubmit={this.handleSaveRequest}>
+
+            <pre>{this.props.query}</pre>
+
+            <div className="form-group">
+              <label className="col-sm-4 text-right" htmlFor="query-name">Query Name</label>
+              <div className="col-sm-8">
+                <input ref="name" name="query-name" type="text" className="form-control" />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label className="col-sm-4 text-right" htmlFor="query-description">Query Description</label>
+              <div className="col-sm-8">
+                <textarea ref="description" rows="4" name="query-description" className="form-control"></textarea>
+              </div>
+            </div>
+
+          </form>
+        </div>
 
         <div className="modal-footer">
-            <div>
+            <div ref="saveQueryIndicator" className="hidden">
               <span className="glyphicon glyphicon-repeat"></span> Saving query...
             </div>
 
-            <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
-            <button type="button" className="btn btn-primary">Save Query</button>
+            <button type="button" className="btn btn-default" onClick={this.props.onRequestHide}>Cancel</button>
+            <button type="button" className="btn btn-primary" onClick={this.handleSaveRequest}>Save Query</button>
         </div>
       </Modal>
     );
+  },
+
+  handleSaveRequest: function(event) {
+    event.preventDefault();
+
+    // Extract the data from the view and pass the data to the
+    // view action to create a new query
+    var name = this.refs['name'].getDOMNode().value,
+        description = this.refs['description'].getDOMNode().value;
+    if ( name == '' ) return;
+
+    // Start the "saving process"
+    $(this.refs['saveQueryIndicator'].getDOMNode()).removeClass('hidden');
+
+    // Send the data await
+    QueryActions.createQuery({
+      query: this.props.query,
+      name: name,
+      description: description
+    });
+  },
+
+  _onChange: function() {
+    console.log('on change');
   }
 });
 
