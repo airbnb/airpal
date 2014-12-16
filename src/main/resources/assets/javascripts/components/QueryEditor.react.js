f962f591fb1a2c69af31903df5aa1d77ed20d53d
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-12-16 13:52:56 +0100

    Adds the ace editor
    Removed the brace editor earlier, but this is by far the best online
    editor out there. So it’s back :D!

--- src/main/resources/assets/javascripts/components/QueryEditor.react.js
+++ src/main/resources/assets/javascripts/components/QueryEditor.react.js
@@ -5,32 +5,42 @@ var React = require('react');
 var QuerySaveModal = require('./QuerySaveModal.react');
 
 /* Helpers */
-var CodeMirror = require('codemirror'),
-    OverlayMixin  = require('react-bootstrap').OverlayMixin;
+var OverlayMixin  = require('react-bootstrap').OverlayMixin,
+    _ = require('lodash');
 
-require('codemirror/mode/sql/sql');
+/* Editor */
+var ace = require('brace'),
+    Placeholder = ace.acequire('ace/placeholder').Placeholder,
+    Range = ace.acequire('ace/range').Range;
+
+require('brace/theme/monokai');
+require('brace/mode/sql');
 
 var QueryEditor = React.createClass({
   displayName: 'QueryEditor',
   mixins: [OverlayMixin],
 
   componentDidMount: function() {
-    this.editor = CodeMirror.fromTextArea(this.refs.queryEditor.getDOMNode(), {
-      lineNumbers: true,
-      tabSize: 2,
-      mode: 'text/x-mysql',
-      theme: 'blackboard'
-    });
+
+    // Create the editor
+    this.editor = ace.edit(this.refs.queryEditor.getDOMNode());
+
+    // Set the theme and the mode
+    this.editor.setTheme('ace/theme/monokai');
+    this.editor.getSession().setMode('ace/mode/sql');
+
+    // Listen to the selection event
+    this.editor.selection.on('changeSelection', _.debounce(
+      this.handleChangeSelection, 150, { maxWait: 150 }
+    ));
+
   },
 
   getInitialState: function() {
-    return { isModalOpen: false };
-  },
-
-  handleToggle: function () {
-    this.setState({
-      isModalOpen: !this.state.isModalOpen
-    });
+    return {
+      isModalOpen: false,
+      runText: 'query'
+    };
   },
 
   render: function () {
@@ -43,8 +53,17 @@ var QueryEditor = React.createClass({
               <h3 className="panel-title">Query editor</h3>
             </div>
             <div className="panel-body">
-              <div className="col-sm-12">
-                <textarea ref="queryEditor" name="query-editor"></textarea>
+              <div ref="queryContainer" className="col-sm-12 editor-container">
+
+                <pre ref="queryEditor" className="editor">
+                  SELECT COUNT(1) FROM users
+                </pre>
+
+                <div ref="handle" className="editor-resize-handles">
+                  <span className="glyphicon glyphicon-chevron-up editor-resize-handle" onClick={this.handleResizeShrink} title="Shrink Editor"></span>
+                  <span className="glyphicon glyphicon-chevron-down editor-resize-handle" onClick={this.handleResizeGrow} title="Grow Editor"></span>
+                </div>
+
               </div>
             </div>
           </div>
@@ -61,11 +80,11 @@ var QueryEditor = React.createClass({
             <div className="col-sm-6 text-right">
               <div className="btn-toolbar pull-right">
                 <div className="btn-group">
-                  <button className="btn btn-primary" onClick={this.handleToggle}>Save Query</button>
+                  <button className="btn btn-primary" onClick={this.handleToggle}>Save {this.state.runText}</button>
                 </div>
 
                 <div className="btn-group">
-                  <button className="btn btn-success">Run Query</button>
+                  <button className="btn btn-success">Run {this.state.runText}</button>
                 </div>
               </div>
             </div>
@@ -80,7 +99,84 @@ var QueryEditor = React.createClass({
     if( !this.state.isModalOpen ) return(<span />);
 
     // Render the modal when it's needed
-    return (<QuerySaveModal onRequestHide={this.handleToggle} query={this.editor.getValue()} />);
+    return (<QuerySaveModal onRequestHide={this.handleToggle} query={this._getQuery()} />);
+  },
+
+  // - Internal events ----------------------------------------------------- //
+  handleToggle: function () {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  },
+
+  handleChangeSelection: function() {
+    var range = this.editor.selection.getRange();
+
+    // Define the text based on the current text range
+    var text = ( !this._isRangeStartSameAsEnd(range) ) ? 'selection' : 'query';
+    if ( text === this.state.runText ) return;
+
+    // Update the state with the new runText
+    this.setState({ runText: text });
+  },
+
+  handleResizeShrink: function($event) {
+    this._resizeEditor(-120);
+  },
+
+  handleResizeGrow: function($event) {
+    this._resizeEditor(120);
+  },
+
+  // - Internal helpers ---------------------------------------------------- //
+
+  // Resizes the editor based on the given pixels
+  // @param {integer} the amount of pixels to increase/decrease the editor
+  _resizeEditor: function(pixels) {
+
+    // Get the editor by className
+    var $el = $(this.refs.queryEditor.getDOMNode());
+
+    // Animate the editor height
+    $el.animate({
+      height: $el.height() + pixels },
+      {
+        duration: 300,
+
+        // Notify the AceEditor on completion that it has resized
+        complete: function() {
+          this.editor.resize(true);
+        }.bind(this)
+      }
+    );
+  },
+
+  // Retrieves the current query
+  // @return {string} the query string
+  _getQuery: function() {
+    var query, range = this.editor.selection.getRange();
+
+    // Define the current query
+    if ( !this._isRangeStartSameAsEnd(range) ) {
+      query = this.editor.session.getTextRange(range);
+    } else {
+      query = this.editor.getValue();
+    }
+
+    // Return the query value
+    return query;
+  },
+
+  // Checks or there is currently something selected
+  // @return {boolean} is the start equal to the end of the selection
+  _isRangeStartSameAsEnd: function(range) {
+    var start = range.start,
+        end   = range.end;
+
+    // Return of the start equals the end of the selection
+    return !!start && !!end &&
+      (start.row === end.row) &&
+      (start.column === end.column);
   }
 });
 
