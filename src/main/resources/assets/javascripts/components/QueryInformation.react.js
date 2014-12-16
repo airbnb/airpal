cecbab2a4ad6aa140b6c6e3f4c44f30a9cefcfed
Stefan Vermaas <stefan.vermaas@nedap.com>
2014-12-16 16:46:22 +0100

    Basic setup for the query info view

---
+++ src/main/resources/assets/javascripts/components/QueryInformation.react.js
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var QueryInformation = React.createClass({
+  displayName: 'QueryInformation',
+  render: function () {
+    return (
+      <div>QueryInformation</div>
+    );
+  }
+});
+
+module.exports = QueryInformation;
