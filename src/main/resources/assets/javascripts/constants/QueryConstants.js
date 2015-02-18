b3500bfa1a979cfeb0953463b721050fac6be39f
Spike Brehm <ocelot@gmail.com>
2015-02-17 19:46:58 -0800

    Remove updateQuery action & related code
    It turns out we're not implementing a PATCH for the queries resource,
    and we won't be using the `updateQuery` action or related code. Let's
    nuke it to simplify things.

--- src/main/resources/assets/javascripts/constants/QueryConstants.js
+++ src/main/resources/assets/javascripts/constants/QueryConstants.js
@@ -6,12 +6,10 @@ var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
   CREATE_QUERY: null,
-  UPDATE_QUERY: null,
   DESTROY_QUERY: null,
 
   RECEIVED_SINGLE_QUERY: null,
   RECEIVED_MULTIPLE_QUERIES: null,
-  RECEIVED_UPDATED_QUERY: null,
   RECEIVED_DESTROYED_QUERY: null,
 
   FETCH_USER_QUERIES: null
