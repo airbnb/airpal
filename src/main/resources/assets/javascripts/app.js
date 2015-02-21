5a65bd39fbec5e16ad1ed8a76202f4f7eb862363
Josh Perez <josh.perez@airbnb.com>
2015-02-20 17:12:57 -0800

    Adds alt

--- src/main/resources/assets/javascripts/app.js
+++ src/main/resources/assets/javascripts/app.js
@@ -8,9 +8,6 @@ Object.assign = require('object-assign');
 var AirpalApp = require('./components/AirpalApp');
 import React from 'react';
 
-// Export for http://fb.me/react-devtools
-window.React = React;
-
 // Start the main app
 React.render(
   <AirpalApp />,
