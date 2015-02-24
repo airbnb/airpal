afcbe48207b0672ba8490f6207689a2de471cc85
Harrison Shoff <hi@hshoff.com>
2015-02-24 02:10:36 -0800

    [ui] drop the branding

--- src/main/resources/assets/javascripts/components/LeftPane.jsx
+++ src/main/resources/assets/javascripts/components/LeftPane.jsx
@@ -6,9 +6,6 @@ let LeftPane = React.createClass({
     return (
       <div className='flex flex-column flex-initial left-pane'>
         <div className='info'>
-          <h3 className='abel brand'>
-            AIRPAL
-          </h3>
           <div className='flex flex-row flex-initial'>
             <div className='flex flex-column'>
               <div>
