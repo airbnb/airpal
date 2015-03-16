423a42464769e3c7c20475fbf3cfdc1f1e64ce07
Josh Perez <josh.perez@airbnb.com>
2015-03-16 16:39:35 -0700

    Moving async to actions for Run

--- src/main/resources/assets/javascripts/stores/RunStore.js
+++ src/main/resources/assets/javascripts/stores/RunStore.js
@@ -2,12 +2,44 @@ import alt from '../alt';
 import FluxCollection from '../utils/FluxCollection';
 import RunActions from '../actions/RunActions';
 import TabActions from '../actions/TabActions';
-import RunApiUtils from '../utils/RunApiUtils';
-import logError from '../utils/logError'
+
+// Yeah baby. We're ready to rambo! The SSEConnection has made a connection
+// to the API endpoint and now we should start getting updates (if any runs
+// are running of course).
+const handleOpen = function () {
+  RunActions.handleConnectionOpen();
+};
+
+// The SSEConnection received an error. Notify the user about this error.
+// @param event {Object} the error object from the API
+const handleError = function (event) {
+  RunActions.handleConnectionError(event);
+};
+
+// The SSEConnection has received a message from the API. We should notify
+// the application on this.
+// @param event {Object} the event object from the API
+const handleMessage = function (event) {
+  var data = JSON.parse(event.data);
+  RunActions.handleConnectionMessage(data);
+};
 
 class RunStore {
   constructor() {
-    this.bindActions(RunActions);
+    this.bindListeners({
+      onConnect: [RunActions.WENT_ONLINE, RunActions.CONNECT],
+      onDisconnect: [RunActions.WENT_OFFLINE, RunActions.DISCONNECT],
+      onResetOnlineStatus: RunActions.RESET_ONLINE_STATUS,
+      onAddMultipleRuns: RunActions.ADD_MULTIPLE_RUNS,
+      onAddRun: RunActions.ADD_RUN,
+      onMessage: RunActions.HANDLE_MESSAGE,
+      onFetchHistory: RunActions.FETCH_HISTORY,
+      onExecute: RunActions.EXECUTE
+    });
+
+    this.exportPublicMethods({
+      getCollection: this.getCollection
+    });
 
     this.collection = new FluxCollection({
       comparator: (model) => -model.queryStarted
@@ -20,37 +52,35 @@ class RunStore {
 
   // Creates an SSE connection to the backend to make a real time stream
   // with the API
-  connect() {
+  onConnect() {
     this.disconnect(); // Close any open connection
 
     // Create a new listener to the API endpoint
     this._eventSource = new EventSource('/api/updates/subscribe');
 
     // Listen to incoming messages
-    this._eventSource.addEventListener('open', this.handleOpen.bind(this));
-    this._eventSource.addEventListener('error', this.handleError.bind(this));
-    this._eventSource.addEventListener('message', this.handleMessage.bind(this));
+    this._eventSource.addEventListener('open', handleOpen);
+    this._eventSource.addEventListener('error', handleError);
+    this._eventSource.addEventListener('message', handleMessage);
 
     this.online = true;
     this.offline = false;
   }
 
   // Close the open SSE connect
-  disconnect() {
+  onDisconnect() {
     if (!!this._eventSource && this._eventSource.readyState) {
       this._eventSource.close();
     }
 
+    if (this._eventSource) {
+      this._eventSource.removeEventListener('open', handleOpen);
+      this._eventSource.removeEventListener('error', handleError);
+      this._eventSource.removeEventListener('message', handleMessage);
+    }
+
     this.online = false;
     this.offline = true;
-  }
-
-  onWentOnline() {
-    this.connect();
-  }
-
-  onWentOffline() {
-    this.disconnect();
   }
 
   onResetOnlineStatus() {
@@ -73,50 +103,13 @@ class RunStore {
   onFetchHistory() {
     if (this.hasFetchedHistory) return;
     this.hasFetchedHistory = true;
-
-    RunApiUtils.fetchHistory().then((results) => {
-      RunActions.addMultipleRuns(results);
-    }).catch(logError);
   }
 
-  onExecute({ query, tmpTable }) {
-    RunApiUtils.execute(query, tmpTable).then((runObject) => {
-      TabActions.selectTab(1);
-      RunActions.addRun(runObject);
-    }).catch(logError);
-
-    // Do not emit event
-    return false;
+  onExecute() {
+    TabActions.selectTab(1);
   }
 
-  onKill(uuid) {
-    RunApiUtils.kill(uuid);
-
-    return false;
-  }
-
-  // Yeah baby. We're ready to rambo! The SSEConnection has made a connection
-  // to the API endpoint and now we should start getting updates (if any runs
-  // are running of course).
-  handleOpen() {
-    RunActions.onOpen();
-  }
-
-  // The SSEConnection received an error. Notify the user about this error.
-  // @param event {Object} the error object from the API
-  handleError(event) {
-    RunActions.onError(event);
-  }
-
-  // The SSEConnection has received a message from the API. We should notify
-  // the application on this.
-  // @param event {Object} the event object from the API
-  handleMessage(event) {
-    var data = JSON.parse(event.data);
-    RunActions.onMessage(data);
-  }
-
-  static getCollection() {
+  getCollection() {
     return this.getState().collection;
   }
 }
