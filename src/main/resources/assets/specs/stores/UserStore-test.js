99be301f4fd037df5e5b80e843e0a0df1ae50639
Josh Perez <josh.perez@airbnb.com>
2015-03-20 16:50:59 -0700

    Passing specs

--- src/main/resources/assets/specs/stores/UserStore-test.js
+++ src/main/resources/assets/specs/stores/UserStore-test.js
@@ -6,8 +6,6 @@ describe('UserStore', () => {
   const UserStore = require('../../javascripts/stores/UserStore');
 
   it('returns a default user', () => {
-    console.info('it returns a default user');
-
     const currentUser = UserStore.getCurrentUser();
     expect(currentUser['name']).toEqual('unknown');
   });
