8661bf9c5b7e604a82eb5f405dc464df2c83ecb7
Greg Wittel <gwittel@proofpoint.com>
2016-04-29 12:53:01 -0700

    Remove use of Jetbrains annotation

--- src/main/java/com/airbnb/airpal/core/execution/ExecutionClient.java
+++ src/main/java/com/airbnb/airpal/core/execution/ExecutionClient.java
@@ -26,12 +26,11 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.inject.Inject;
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 
 import javax.annotation.Nullable;
-
+import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
